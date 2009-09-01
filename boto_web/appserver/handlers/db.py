d84958202d63e046f9c2caa870dccdb0fbce9f61
kopertop <devnull@localhost>
2009-09-01 21:00:50 +0000

    Fixed some minor issues with updating in db handler

--- boto_web/appserver/handlers/db.py
+++ boto_web/appserver/handlers/db.py
@@ -18,231 +18,235 @@ from boto_web import xmlize
 
 
 try:
-    import json
+	import json
 except ImportError:
-    import simplejson as json
+	import simplejson as json
 
 class DBHandler(RequestHandler):
-    """
-    DB Handler Base class
-    this provides a simple CRUD interface to 
-    any DB object
+	"""
+	DB Handler Base class
+	this provides a simple CRUD interface to 
+	any DB object
 
-    The simplest use of this handler would be calling it directly as such::
-        - url: /blog
-          handler: boto_web.appserver.handlers.db.DBHandler
-          db_class: resources.post.Post
+	The simplest use of this handler would be calling it directly as such::
+		- url: /blog
+		  handler: boto_web.appserver.handlers.db.DBHandler
+		  db_class: resources.post.Post
 
-    You may also pass in the follwoing custom fields:
-    * db_class: Required, the class to use for this interface
-    """
-    db_class = None
+	You may also pass in the follwoing custom fields:
+	* db_class: Required, the class to use for this interface
+	"""
+	db_class = None
 
-    def __init__(self, env, config):
-        RequestHandler.__init__(self, env, config)
-        db_class_name = self.config.get('db_class', None)
-        if db_class_name:
-            self.db_class = find_class(db_class_name)
-            print "Loaded db class: %s" % db_class_name
-        xmlize.register(self.db_class)
+	def __init__(self, env, config):
+		RequestHandler.__init__(self, env, config)
+		db_class_name = self.config.get('db_class', None)
+		if db_class_name:
+			self.db_class = find_class(db_class_name)
+		xmlize.register(self.db_class)
 
-    def _get(self, request, response, id=None ):
-        """Get an object, or search for a list of objects"""
-        response.content_type = "text/xml"
-        if id:
-            vals = id.split("/",1)
-            property = None
-            if len(vals) > 1:
-                (id, property) = vals
-            obj = self.read(id=id, user=request.user)
-            if property:
-                return self.get_property(response, obj, property)
-            else:
-                response.write(xmlize.dumps(obj))
-        else:
-            objs = self.search(params=request.GET.mixed(), user=request.user)
-            response.write("<%sList>" % self.db_class.__name__)
-            for obj in objs:
-                response.write(xmlize.dumps(obj))
-            response.write("</%sList>" % self.db_class.__name__)
-        return response
+	def _get(self, request, response, id=None ):
+		"""Get an object, or search for a list of objects"""
+		response.content_type = "text/xml"
+		if id:
+			vals = id.split("/",1)
+			property = None
+			if len(vals) > 1:
+				(id, property) = vals
+			obj = self.read(id=id, user=request.user)
+			if property:
+				return self.get_property(response, obj, property)
+			else:
+				response.write(xmlize.dumps(obj))
+		else:
+			objs = self.search(params=request.GET.mixed(), user=request.user)
+			response.write("<%sList>" % self.db_class.__name__)
+			obj_count = 0
+			for obj in objs:
+				obj_count += 1
+				response.write(xmlize.dumps(obj))
+				if obj_count > 50:
+					response.write("<more_results/>")
+					break
+			response.write("</%sList>" % self.db_class.__name__)
+		return response
 
-    def _post(self, request, response, id=None):
-        """Create a new resource"""
-        if id:
-            obj = self.db_class.get_by_ids(id)
-            if obj:
-                raise Conflict("Object %s already exists" % id)
+	def _post(self, request, response, id=None):
+		"""Create a new resource"""
+		if id:
+			obj = self.db_class.get_by_ids(id)
+			if obj:
+				raise Conflict("Object %s already exists" % id)
 
-        new_obj = xmlize.loads(request.body)
-        new_obj.id = id
-        obj = self.create(new_obj, request.user)
-        response.set_status(201)
-        response.headers['Location'] = obj.id
-        return response
-
-
-    def _put(self, request, response, id=None):
-        """Update an existing resource"""
-        content = None
-        obj = None
-        if id:
-            obj = self.db_class.get_by_ids(id)
-
-        if not obj:
-            raise NotFound()
-
-        new_obj = xmlize.loads(request.body)
-        props = {}
-        for prop in dir(new_obj):
-            prop_value = getattr(new_obj, prop)
-            if not prop.startswith("_") and not type(prop_value) == type(self._put):
-                props[prop] = prop_value
-        content =  self.update(obj, props, request.user)
-
-        response.content_type = "text/xml"
-        content.to_xml().writexml(response)
-        return response
+		new_obj = xmlize.loads(request.body)
+		new_obj.id = id
+		obj = self.create(new_obj, request.user)
+		response.set_status(201)
+		response.headers['Location'] = obj.id
+		return response
 
 
-    def _delete(self, request, response, id=None):
-        """
-        Delete a given object
-        """
-        obj = self.read(id=id, user=request.user);
-        content = self.delete(obj,user=request.user)
-        response.content_type = "text/xml"
-        content.to_xml().writexml(response)
-        return response
+	def _put(self, request, response, id=None):
+		"""Update an existing resource"""
+		content = None
+		obj = None
+		if id:
+			obj = self.db_class.get_by_ids(id)
 
-    def decode(self, type, value):
-        """
-        Decode a string value sent by the user into actual things
-        """
-        if value:
-            if Model in type.mro():
-                return type.get_by_ids(value)
-            elif type == bool:
-                if value.lower() == 'true':
-                    return True
-                else:
-                    return False
-            elif type == datetime:
-                return datetime.strptime(value, "%m/%d/%y %H:%M:%S")
-            elif type == Password:
-                return value
-            else:
-                return type(value)
-        return value
+		if not obj:
+			raise NotFound()
 
-    ###
-    # The CRUD interface, this is
-    # all you should override if you need to
-    ##
-    def search(self, params, user):
-        """
-        Search for given objects
-        @param params: The Terms to search for
-        @type params: Dictionary
+		new_obj = xmlize.loads(request.body)
+		props = {}
+		for prop in dir(new_obj):
+			prop_value = getattr(new_obj, prop)
+			if not prop.startswith("_") and not prop == "id" and not type(prop_value) == type(self._put):
+				props[prop] = prop_value
+		content =  self.update(obj, props, request.user)
 
-        @param user: the user that is searching
-        @type user: User
-        """
-        query_str = params.get("query", None)
-        query = self.db_class.find()
-        sort_by = params.get("sort_by", None)
-        if query_str:
-            try:
-                filters = json.loads(query_str)
-            except:
-                raise BadRequest("Bad query string")
-            for filter in filters:
-                query.filter("%s %s" % (filter[0], filter[1]), filter[2])
-        else:
-            properties = [p.name for p in self.db_class.properties(hidden=False)]
-            for filter in set(params.keys()):
-                if filter == "sort_by":
-                    continue
-                if not filter in properties:
-                    raise BadRequest("Property not found: '%s'" % filter)
-                filter_value = params[filter]
-                filter_args = filter.split(".")
-                if len(filter_args) > 1:
-                    filter_cmp = filter_args[1]
-                else:
-                    filter_cmp = "="
-                filter = filter_args[0]
-                if len(filter_value) == 1:
-                    filter_value = filter_value[0]
-                if filter_value:
-                    query.filter("%s %s " % (filter, filter_cmp), filter_value)
-        if sort_by:
-            query.order(sort_by)
-        return query
+		response.content_type = "text/xml"
+		content.to_xml().writexml(response)
+		return response
 
-    def create(self, obj, user):
-        """
-        Create an object in the DB
-        @param obj: The object to create
-        @type obj: self.db_class
-        @param user: The user doing the creation
-        @type user: User
-        """
-        obj.put()
-        return obj
 
-    def read(self, id, user):
-        """
-        Get the object that this URI points to, or None if they don't point to one
-        If we point to a URI that doesn't exist, we toss a NotFound error
-        """
-        obj = None
-        try:
-            obj = self.db_class.get_by_ids(id)
-        except:
-            raise NotFound()
-        if not obj:
-            raise NotFound()
-        return obj
+	def _delete(self, request, response, id=None):
+		"""
+		Delete a given object
+		"""
+		obj = self.read(id=id, user=request.user);
+		content = self.delete(obj,user=request.user)
+		response.content_type = "text/xml"
+		content.to_xml().writexml(response)
+		return response
 
-    def update(self, obj, props, user):
-        """
-        Update our object
+	def decode(self, type, value):
+		"""
+		Decode a string value sent by the user into actual things
+		"""
+		if value:
+			if Model in type.mro():
+				return type.get_by_ids(value)
+			elif type == bool:
+				if value.lower() == 'true':
+					return True
+				else:
+					return False
+			elif type == datetime:
+				return datetime.strptime(value, "%m/%d/%y %H:%M:%S")
+			elif type == Password:
+				return value
+			else:
+				return type(value)
+		return value
 
-        @param obj: Object to update
-        @type obj: self.db_class
+	###
+	# The CRUD interface, this is
+	# all you should override if you need to
+	##
+	def search(self, params, user):
+		"""
+		Search for given objects
+		@param params: The Terms to search for
+		@type params: Dictionary
 
-        @param props: A has of properties to update
-        @type props: hash
+		@param user: the user that is searching
+		@type user: User
+		"""
+		query_str = params.get("query", None)
+		query = self.db_class.find()
+		sort_by = params.get("sort_by", None)
+		if query_str:
+			try:
+				filters = json.loads(query_str)
+			except:
+				raise BadRequest("Bad query string")
+			for filter in filters:
+				query.filter("%s %s" % (filter[0], filter[1]), filter[2])
+		else:
+			properties = [p.name for p in self.db_class.properties(hidden=False)]
+			for filter in set(params.keys()):
+				if filter == "sort_by":
+					continue
+				if not filter in properties:
+					raise BadRequest("Property not found: '%s'" % filter)
+				filter_value = params[filter]
+				filter_args = filter.split(".")
+				if len(filter_args) > 1:
+					filter_cmp = filter_args[1]
+				else:
+					filter_cmp = "="
+				filter = filter_args[0]
+				if len(filter_value) == 1:
+					filter_value = filter_value[0]
+				if filter_value:
+					query.filter("%s %s " % (filter, filter_cmp), filter_value)
+		if sort_by:
+			query.order(sort_by)
+		return query
 
-        @param user: The user making these changes
-        @type user: User
-        """
-        boto.log.debug("===========================")
-        boto.log.debug("Update %s" % obj.__class__.__name__)
-        for prop_name in props:
-            prop_val = props[prop_name]
-            boto.log.debug("%s = %s" % (prop_name, prop_val))
-            setattr(obj, prop_name, prop_val)
-        boto.log.debug("===========================")
-        obj.put()
-        return obj
+	def create(self, obj, user):
+		"""
+		Create an object in the DB
+		@param obj: The object to create
+		@type obj: self.db_class
+		@param user: The user doing the creation
+		@type user: User
+		"""
+		obj.put()
+		return obj
 
-    def delete(self, obj, user):
-        """
-        Delete the object
-        """
-        log.info("Deleted object %s" % (obj.id))
-        obj.delete()
-        return obj
+	def read(self, id, user):
+		"""
+		Get the object that this URI points to, or None if they don't point to one
+		If we point to a URI that doesn't exist, we toss a NotFound error
+		"""
+		obj = None
+		try:
+			obj = self.db_class.get_by_ids(id)
+		except:
+			raise NotFound()
+		if not obj:
+			raise NotFound()
+		return obj
 
-    def get_property(self, response, obj, property):
-        """Return just a single property"""
-        response.content_type = "text/plain"
-        val = getattr(obj, property)
-        if type(val) == list:
-            for v in val:
-                response.write(str(v))
-        else:
-            response.write(str(val))
-        return response
+	def update(self, obj, props, user):
+		"""
+		Update our object
+
+		@param obj: Object to update
+		@type obj: self.db_class
+
+		@param props: A has of properties to update
+		@type props: hash
+
+		@param user: The user making these changes
+		@type user: User
+		"""
+		boto.log.info("===========================")
+		boto.log.info("Update %s" % obj.__class__.__name__)
+		for prop_name in props:
+			prop_val = props[prop_name]
+			boto.log.info("%s = %s" % (prop_name, prop_val))
+			setattr(obj, prop_name, prop_val)
+		boto.log.info("===========================")
+		obj.put()
+		return obj
+
+	def delete(self, obj, user):
+		"""
+		Delete the object
+		"""
+		log.info("Deleted object %s" % (obj.id))
+		obj.delete()
+		return obj
+
+	def get_property(self, response, obj, property):
+		"""Return just a single property"""
+		response.content_type = "text/plain"
+		val = getattr(obj, property)
+		if type(val) == list:
+			for v in val:
+				response.write(str(v))
+		else:
+			response.write(str(val))
+		return response
--- boto_web/xmlize.py
+++ boto_web/xmlize.py
@@ -25,201 +25,199 @@ REGISTERED_CLASSES = {} # A mapping of name=> class for what to decode objects i
 from boto_web.fixed_datetime import datetime
 
 class DefaultObject(object):
-    """Default object for when re get something that we don't know about yet"""
-    id = None
-    __name__ = None
+	"""Default object for when re get something that we don't know about yet"""
+	id = None
+	__name__ = None
 
 from lxml import etree
 class XMLSerializer(object):
-    """XML Serializer object"""
+	"""XML Serializer object"""
 
-    def __init__(self, file=None):
-        """Create a new serialization file"""
-        if not file:
-            from tempfile import TemporaryFile
-            file = TemporaryFile()
-        self.file = file
-        self.num_objs = 0
+	def __init__(self, file=None):
+		"""Create a new serialization file"""
+		if not file:
+			from tempfile import TemporaryFile
+			file = TemporaryFile()
+		self.file = file
+		self.num_objs = 0
 
-    def encode(self, prop_name, prop_value):
-        """Encode this value to XML"""
-        if prop_value == None:
-            return None
-        prop_type = type(prop_value)
-        if prop_type in self.type_map:
-            return self.type_map[prop_type](self, prop_name, prop_value)
-        if isinstance(prop_value, object) and hasattr(prop_value, "id"):
-            return self.encode_object(prop_name, prop_value)
-        return self.encode_default(prop_name, prop_value, prop_type.__name__)
+	def encode(self, prop_name, prop_value):
+		"""Encode this value to XML"""
+		if prop_value == None:
+			return None
+		prop_type = type(prop_value)
+		if prop_type in self.type_map:
+			return self.type_map[prop_type](self, prop_name, prop_value)
+		if isinstance(prop_value, object) and hasattr(prop_value, "id"):
+			return self.encode_object(prop_name, prop_value)
+		return self.encode_default(prop_name, prop_value, prop_type.__name__)
 
-    def encode_default(self, prop_name, prop_value, prop_type):
-        """Encode Default encoding property"""
-        args = {"prop_name": prop_name, "prop_value": self.encode_cdata(prop_value), "prop_type": str(prop_type)}
-        self.file.write("""<%(prop_name)s type="%(prop_type)s">%(prop_value)s</%(prop_name)s>""" % args)
+	def encode_default(self, prop_name, prop_value, prop_type):
+		"""Encode Default encoding property"""
+		args = {"prop_name": prop_name, "prop_value": self.encode_cdata(prop_value), "prop_type": str(prop_type)}
+		self.file.write("""<%(prop_name)s type="%(prop_type)s">%(prop_value)s</%(prop_name)s>""" % args)
 
-    def encode_str(self, prop_name, prop_value):
-        return self.encode_default(prop_name, str(prop_value), "string")
+	def encode_str(self, prop_name, prop_value):
+		return self.encode_default(prop_name, str(prop_value), "string")
 
-    def encode_list(self, prop_name, prop_value):
-        """Encode a list by encoding each property individually"""
-        for val in prop_value:
-            self.encode(prop_name, val)
+	def encode_list(self, prop_name, prop_value):
+		"""Encode a list by encoding each property individually"""
+		for val in prop_value:
+			self.encode(prop_name, val)
 
-    def encode_dict(self, prop_name, prop_value):
-        """Encode a dict by encoding each element individually with a name="" param"""
-        self.file.write("""<%s type="complexType">""" % prop_name)
-        for k in prop_value.keys():
-            v = prop_value[k]
-            self.encode(str(k), v)
-        self.file.write("""</%s>""" % prop_name)
+	def encode_dict(self, prop_name, prop_value):
+		"""Encode a dict by encoding each element individually with a name="" param"""
+		self.file.write("""<%s type="complexType">""" % prop_name)
+		for k in prop_value.keys():
+			v = prop_value[k]
+			self.encode(str(k), v)
+		self.file.write("""</%s>""" % prop_name)
 
-    def encode_datetime(self, prop_name, prop_value):
-        """Encode a DATETIME into standard ISO 8601 Internet Format"""
-        self.encode_default(prop_name, prop_value.isoformat(), "dateTime")
+	def encode_datetime(self, prop_name, prop_value):
+		"""Encode a DATETIME into standard ISO 8601 Internet Format"""
+		self.encode_default(prop_name, prop_value.isoformat(), "dateTime")
 
-    def encode_object(self, prop_name, prop_value):
-        """Encode a generic object (must have an "id" attribute)"""
-        self.encode_default(prop_name, prop_value.id, prop_value.__class__.__name__)
+	def encode_object(self, prop_name, prop_value):
+		"""Encode a generic object (must have an "id" attribute)"""
+		self.encode_default(prop_name, prop_value.id, prop_value.__class__.__name__)
 
-    def encode_cdata(self, string):
-        """Return what might be a CDATA encoded string"""
-        string = str(string)
-        for ch in BAD_CHARS:
-            if ch in string:
-                return "<![CDATA[ %s ]]>" % string
-        return string
-
-
-    type_map = {
-        str: encode_str,
-        unicode: encode_str,
-        list: encode_list,
-        dict: encode_dict,
-        datetime: encode_datetime,
-        object: encode_object,
-    }
+	def encode_cdata(self, string):
+		"""Return what might be a CDATA encoded string"""
+		string = str(string)
+		for ch in BAD_CHARS:
+			if ch in string:
+				return "<![CDATA[ %s ]]>" % string
+		return string
 
 
-    def dump(self, obj):
-        """Dump this object to our serialization"""
-        if not isinstance(obj, object):
-            self.encode(obj.__name__, obj)
-        else:
-            if hasattr(obj, "id") and obj.id:
-                self.file.write("""<%s id="%s">""" % (obj.__class__.__name__, obj.id))
-            else:
-                self.file.write("<%s>" % obj.__class__.__name__)
-            for prop_name in dir(obj):
-                if not prop_name.startswith("_") and not prop_name == "id":
-                    prop_value = getattr(obj, prop_name)
-                    if not type(prop_value) == type(self.dump):
-                        self.encode(prop_name, prop_value)
-            self.file.write("</%s>" % obj.__class__.__name__)
-
-    def load(self):
-        """Load from this file to an object or object list"""
-        self.file.seek(0)
-        tree = etree.parse(self.file)
-        root = tree.getroot()
-        return self.decode(root)
-
-    def decode(self, node):
-        """Decode this node into an object or list of objects"""
-        if node.tag in REGISTERED_CLASSES.keys():
-            model_class = REGISTERED_CLASSES[node.tag]
-            obj = model_class()
-            obj.id = node.get("id")
-            props = {}
-            for prop in node:
-                value = None
-                if prop.get("type") == "string":
-                    value = self.decode_string(prop)
-                elif prop.get("type") == "complexType":
-                    # Dictionary
-                    pass
-                elif prop.get("type") == "dateTime":
-                    # Date Time
-                    value = self.decode_datetime(prop)
-                elif prop.get("type") == "bool":
-                    # Boolean
-                    value = (self.decode_string(prop).upper() == "TRUE")
-                elif prop.get("type") in REGISTERED_CLASSES.keys():
-                    # Object
-                    value = REGISTERED_CLASSES[prop.get("type")]()
-                    value.id = prop.text
-                else:
-                    # An Object we don't know about yet, make it up
-                    value = DefaultObject()
-                    value.id = prop.text
-                    value.__name__ = prop.get("type")
-                if not props.has_key(prop.tag):
-                    props[prop.tag] = []
-                props[prop.tag].append(value)
-            for prop_name in props:
-                val = props[prop_name]
-                if len(val) == 1:
-                    val = val[0]
-                setattr(obj, prop_name, val)
-            return obj
-        elif node.tag.endswith("List"):
-            return [self.decode(x) for x in node]
+	type_map = {
+		str: encode_str,
+		unicode: encode_str,
+		list: encode_list,
+		dict: encode_dict,
+		datetime: encode_datetime,
+		object: encode_object,
+	}
 
 
-    def decode_string(self, node):
-        """Decode a simple string property"""
-        return node.text
+	def dump(self, obj):
+		"""Dump this object to our serialization"""
+		if not isinstance(obj, object):
+			self.encode(obj.__name__, obj)
+		else:
+			if hasattr(obj, "id") and obj.id:
+				self.file.write("""<%s id="%s">""" % (obj.__class__.__name__, obj.id))
+			else:
+				self.file.write("<%s>" % obj.__class__.__name__)
+			for prop_name in dir(obj):
+				if not prop_name.startswith("_") and not prop_name == "id":
+					prop_value = getattr(obj, prop_name)
+					if not type(prop_value) == type(self.dump):
+						self.encode(prop_name, prop_value)
+			self.file.write("</%s>" % obj.__class__.__name__)
 
-    def decode_datetime(self, node):
-        """Decode a simple string property"""
-        date_str = self.decode_string()
-        return datetime.fromisoformat(txt)
+	def load(self):
+		"""Load from this file to an object or object list"""
+		self.file.seek(0)
+		tree = etree.parse(self.file)
+		root = tree.getroot()
+		return self.decode(root)
+
+	def decode(self, node):
+		"""Decode this node into an object or list of objects"""
+		if node.tag in REGISTERED_CLASSES.keys():
+			model_class = REGISTERED_CLASSES[node.tag]
+			obj = model_class()
+			obj.id = node.get("id")
+			props = {}
+			for prop in node:
+				value = None
+				if prop.get("type") == "string":
+					value = self.decode_string(prop)
+				elif prop.get("type") == "complexType":
+					# Dictionary
+					pass
+				elif prop.get("type") == "dateTime":
+					# Date Time
+					value = self.decode_datetime(prop)
+				elif prop.get("type") == "bool":
+					# Boolean
+					value = (self.decode_string(prop).upper() == "TRUE")
+				elif prop.get("type") in REGISTERED_CLASSES.keys():
+					# Object
+					value = REGISTERED_CLASSES[prop.get("type")]()
+					value.id = prop.text
+				else:
+					# By default we assume it's a string
+					value = self.decode_string(prop)
+				if not props.has_key(prop.tag):
+					props[prop.tag] = []
+				props[prop.tag].append(value)
+			for prop_name in props:
+				val = props[prop_name]
+				if len(val) == 1:
+					val = val[0]
+				setattr(obj, prop_name, val)
+			return obj
+		elif node.tag.endswith("List"):
+			return [self.decode(x) for x in node]
+
+
+	def decode_string(self, node):
+		"""Decode a simple string property"""
+		return node.text
+
+	def decode_datetime(self, node):
+		"""Decode a simple string property"""
+		date_str = self.decode_string()
+		return datetime.fromisoformat(txt)
 
 
 
-    
+	
 def dump(obj, file=None):
-    """Write an XML representation of *obj* to the open file object *file*
-    """
-    enc = XMLSerializer(file)
-    enc.dump(obj)
-    enc.file.seek(0)
-    return enc.file
+	"""Write an XML representation of *obj* to the open file object *file*
+	"""
+	enc = XMLSerializer(file)
+	enc.dump(obj)
+	enc.file.seek(0)
+	return enc.file
 
 def dumps(obj):
-    """Dump the XML to a string, this is equivalent to dump(obj).read()"""
-    return dump(obj).read()
+	"""Dump the XML to a string, this is equivalent to dump(obj).read()"""
+	return dump(obj).read()
 
 def load(file):
-    """Read a from the open file object *file* and interpret it as an XML serialization
+	"""Read a from the open file object *file* and interpret it as an XML serialization
 
-    @param file: File object to read from
-    @type file: file
-    """
-    return XMLSerializer(file).load()
+	@param file: File object to read from
+	@type file: file
+	"""
+	return XMLSerializer(file).load()
 
 def loads(string):
-    """Load from a string
-    @param string: String of the XML to parse
-    @type string: str
-    """
-    from StringIO import StringIO
-    return load(StringIO(string))
+	"""Load from a string
+	@param string: String of the XML to parse
+	@type string: str
+	"""
+	from StringIO import StringIO
+	return load(StringIO(string))
 
 def register(cls, name=None):
-    """Register a class to be deserializable
+	"""Register a class to be deserializable
 
-    @param cls: Class to register
-    @type cls: class
+	@param cls: Class to register
+	@type cls: class
 
-    @param name: An optional conanical name to use for this class, the default is to use 
-        just the name of the class
-    @type name: str
-    """
-    if name == None:
-        name = cls.__name__
-    REGISTERED_CLASSES[name] = cls
+	@param name: An optional conanical name to use for this class, the default is to use 
+		just the name of the class
+	@type name: str
+	"""
+	if name == None:
+		name = cls.__name__
+	REGISTERED_CLASSES[name] = cls
 
 def get_class(class_name):
-    """Get the class for this name"""
-    return REGISTERED_CLASSES.get(class_name)
+	"""Get the class for this name"""
+	return REGISTERED_CLASSES.get(class_name)
 
