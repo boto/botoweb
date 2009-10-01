352afae7b7e39fedd6a0b0eed2acb41642778d9f
ipaterson <devnull@localhost>
2009-09-30 21:23:23 -0400

    Reintroduced form generation for object editing. Added BaseModelEditor as an entry point for generating new/edit object interfaces. Includes some test code after loading the environment to display an editing interface for the current User object.

--- clients/javascript/ui.js
+++ clients/javascript/ui.js
@@ -39,21 +39,25 @@ boto_web.ui = {
 		self.pages = {};
 		// Merge homepage as a generated false API
 		$.each($.merge([{href: 'home', name: 'Home', home: 1}], env.apis), function() {
-			var page = new boto_web.ui.Page(this, env);
+			this.page = new boto_web.ui.Section(this, env);
 			$('<a/>')
-				.attr({href: '#' + page.href})
+				.attr({href: '#' + this.page.href})
 				.text(this.name)
 				.appendTo($('<li/>').appendTo(self.nav));
 
-			self.pages[this.href] = page;
+			self.pages[this.href] = this.page;
 		});
+
+		env.models.User.all(function(o) { alert(o.length)} );
+		env.models.User.get(env.user.id, function(o) { o.show_editor(); });
+		//.show_editor();
 	},
 
-	Page: function(api, env) {
+	Section: function(api, env) {
 		var self = this;
 
 		self.api = api;
-		self.href = env.base_url.replace(/([^\/])$/, '$1/') + self.api.href;
+		self.href = '/' + self.api.href;
 		self.name = 'Database Management' + ((api.home) ? '' : ' &ndash; ' + self.api.name);
 
 		self.node = $('<div/>')
@@ -63,7 +67,7 @@ boto_web.ui = {
 			.load(function() { boto_web.ui.heading.html(self.name) })
 			.hide()
 			.appendTo(boto_web.ui.node);
-		var home = $('<div/>')
+		self.content = $('<div/>')
 			.attr({id: self.api.href + '_main'})
 			.addClass('content')
 			.appendTo(self.node)
@@ -76,102 +80,162 @@ boto_web.ui = {
 						.attr({href: '#' + self.href + '/' + i})
 						.text(self.api.methods[i])
 						.addClass('button')
-						.appendTo(home)
+						.appendTo(self.content)
 				default:
 					$('<div/>')
 						.attr({id: self.api.href + '_' + i})
-						.text('Unfinished content placeholder for ' + self.api.name + ' ' + i + ' method page.' )
+						.text('Please wait, loading.')
 						.addClass('content')
 						.hide()
+						.load(function() { alert('loaded') })
 						.appendTo(self.node)
 			}
 		}
 	},
-	///**
-	 //* Generic interface for all field types.
-	 //*
-	 //* @param {Object} properties HTML node properties.
-	 //*/
-	//_field: function(properties) {
-		//var self = this;
-		//this.node = $('<div/>');
-		//this.label = $('<label/>').text(properties._label || '');
-		//this.field = $('<' + (properties._tagName || 'input') + '/>');
-		//this.text = $('<span/>');
 
-		//properties.id = properties.id || 'field_' + properties.name;
+	BaseModelEditor: function(model, obj, opts) {
+		var self = this;
 
-		//for (p in properties) {
-			//var v = properties[p];
+		self.node = $('<div/>')
+			.addClass('content')
+			.appendTo(model.api.page.node);
 
-			//if (p.indexOf('_') == 0)
-				//continue;
+		model.api.page.node.show();
 
-			////TODO More special cases needed (i.e. multiple choice items)
-			//switch (p) {
-				//case 'choices':
-					//for (i in v) {
-						//v[i].text = v[i].text || v[i].value;
-						//var opt = $('<option/>').attr(v[i]);
+		self.properties = model.api.properties;
+		self.obj = obj;
+		self.model = model;
+		self.fields = [];
 
-						//this.field.append(opt)
-					//}
-					//break;
-				//default:
-					//this.field.attr(p, v);
-			//}
-		//}
+		$(self.properties).each(function() {
+			var props = $.extend(this, {value: obj.properties[this.name]});
+			var field;
 
-		//if (properties._default) {
-			//this.field.val(properties._default);
-		//}
+			switch (this.type) {
+				case 'string':
+				case 'integer':
+					field = new boto_web.ui.text(props)
+						.read_only(false);
+					break;
+				case 'dateTime':
+					field = new boto_web.ui.date(props)
+						.read_only(false);
+			}
 
-		//this.text.text(this.field.val());
+			if (typeof field == 'undefined') return;
 
-		///**
-		 //* Switches the
-		 //*/
-		//this.read_only = function(on) {
-			//if (on || on == undefined) {
-				//this.field_container.hide();
-				//this.text.show();
-			//}
-			//else {
-				//this.text.hide();
-				//this.field_container.show();
-			//}
-		//}
+			self.fields.push(field);
+			field.node.appendTo(self.node)
+		});
 
-		//this.field_container = $('<span/>').append(this.field);
-		//this.node.append(this.label, this.field_container, this.text);
-		//this.read_only();
-	//},
+		self.submit = function() {
+			var self = this;
+			var data = {id: self.obj.id};
 
-	///**
-	 //* @param {Object} properties HTML node properties.
-	 //*/
-	//textarea: function(properties) {
-		//properties.innerHTML = properties.value;
-		//boto_web.ui._field.call(this, properties);
-	//},
+			$(self.fields).each(function() {
+				data[this.field.attr('name')] = this.field.val();
+			});
 
-	///**
-	 //* @param {Object} properties HTML node properties.
-	 //*/
-	//text: function(properties) {
-		//boto_web.ui._field.call(this, properties);
-	//},
+			self.model.save(data);
+		};
 
-	///**
-	 //* @param {Object} properties HTML node properties.
-	 //*/
-	//date: function(properties) {
-		//boto_web.ui._field.call(this, properties);
+		$('<input/>')
+			.attr({type: 'submit'})
+			.text('Update')
+			.click(function() { self.submit() })
+			.appendTo(self.node);
+	},
 
-		//this.datepicker = $(this.field).datepicker({
-			//showOn: 'both',
-			//showAnim: 'slideDown'
-		//});
 
-	//}
+	/**
+	 * Generic interface for all field types.
+	 *
+	 * @param {Object} properties HTML node properties.
+	 */
+	_field: function(properties) {
+		var self = this;
+		this.node = $('<div/>');
+		this.label = $('<label/>').text(properties._label || '');
+		this.field = $('<' + (properties._tagName || 'input') + '/>');
+		this.text = $('<span/>');
+
+		properties.id = properties.id || 'field_' + properties.name;
+
+		for (p in properties) {
+			var v = properties[p];
+
+			if (p.indexOf('_') == 0)
+				continue;
+
+			//TODO More special cases needed (i.e. multiple choice items)
+			switch (p) {
+				case 'choices':
+					for (i in v) {
+						v[i].text = v[i].text || v[i].value;
+						var opt = $('<option/>').attr(v[i]);
+
+						this.field.append(opt)
+					}
+					break;
+				default:
+					this.field.attr(p, v);
+			}
+		}
+
+		if (properties._default) {
+			this.field.val(properties._default);
+		}
+
+		this.text.text(this.field.val());
+
+		/**
+		 * Switches the
+		 */
+		this.read_only = function(on) {
+			if (on || typeof on == 'undefined') {
+				this.field_container.hide();
+				this.text.show();
+			}
+			else {
+				this.text.hide();
+				this.field_container.show();
+			}
+
+			return this;
+		}
+
+		this.field_container = $('<span/>').append(this.field);
+		this.node.append(this.label, this.field_container, this.text);
+		this.read_only();
+	},
+
+	/**
+	 * @param {Object} properties HTML node properties.
+	 */
+	textarea: function(properties) {
+		properties.innerHTML = properties.value;
+		boto_web.ui._field.call(this, properties);
+	},
+
+	/**
+	 * @param {Object} properties HTML node properties.
+	 */
+	text: function(properties) {
+		boto_web.ui._field.call(this, properties);
+	},
+
+	/**
+	 * @param {Object} properties HTML node properties.
+	 */
+	date: function(properties) {
+		boto_web.ui._field.call(this, properties);
+
+		this.datepicker = $(this.field).datepicker({
+			showOn: 'both',
+			showAnim: 'slideDown'
+		});
+
+	}
 };
+
+boto_web.Model.prototype.show_editor = function(opts) { return boto_web.ui.BaseModelEditor(boto_web.env.models[this.name], this, opts); }
