4dcc917880de4ddb3f62a3614938af384e31bf3a
Chris Moyer <cmoyer@cloudbasin.com>
2009-10-12 13:20:13 -0400

    Added tests for botoweb xmlizer

--- tests/test_xmlize.py
+++ tests/test_xmlize.py
@@ -19,11 +19,24 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
+import datetime
+import pytz
 from botoweb import xmlize
 
 class ObjectTest(object):
 	"""Test Object to dump/load from XML"""
 	pass
+
+TEST_XML = """<?xml version="1.0" ?>
+<Test>
+	<test_simple>Simple String</test_simple>
+	<test_string type="string">Another String</test_string>
+	<test_datetime type="dateTime">2009-09-09T09:09:09-05</test_datetime>
+	<test_multi type="string">Value A</test_multi>
+	<test_multi type="string">Value B</test_multi>
+	<test_multi type="string">Value C</test_multi>
+</Test>
+"""
 
 class TestXMLize(object):
 	"""Test the xmlize package serializing and de-serializing XML"""
@@ -41,4 +54,29 @@ class TestXMLize(object):
 
 		assert obj2.foo == obj.foo
 		assert obj2.name == obj.name
-		assert obj2.id == obj.id
+		assert obj2.__id__ == obj.id
+
+	def test_datetime(self):
+		"""Try encoding and decoding a datetime"""
+		xmlize.register(ObjectTest)
+		obj = ObjectTest()
+		obj.d = datetime.datetime(year=2009, month=9, day=9, hour=9, minute=9, second=9, tzinfo=pytz.utc)
+		sr = xmlize.dumps(obj)
+		obj2 = xmlize.loads(sr)
+
+		print obj2.d.tzinfo
+		assert obj2.d == obj.d
+
+
+	def test_loads(self):
+		"""Test loading a string that we know should work"""
+		obj = xmlize.loads(TEST_XML)
+		assert obj.test_simple == "Simple String"
+		assert obj.test_string == "Another String"
+		assert obj.test_datetime.year == 2009
+		assert obj.test_datetime.month == 9
+		assert obj.test_datetime.day == 9
+		assert obj.test_datetime.hour == 9
+		assert obj.test_datetime.minute == 9
+		assert obj.test_datetime.second == 9
+		assert obj.test_datetime.tzinfo.utcoffset(obj.test_datetime) == datetime.timedelta(hours = -5)
