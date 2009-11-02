fb39f647a848a4c9e5b2a10154c682cab56627cf
ipaterson <devnull@localhost>
2009-11-02 10:25:09 -0500

    Added quick update trigger for reporting page, must be improved later.

--- clients/javascript/ui/page.js
+++ clients/javascript/ui/page.js
@@ -15,6 +15,9 @@ boto_web.ui.Page = function(html) {
 	self.id = self.node.attr('id') || 'section_' + boto_web.ui.desktop.num_pages;
 
 	if (self.id in boto_web.ui.desktop.pages) {
+		// TODO trigger a real event
+		if ('listener' in boto_web.ui.desktop.pages[self.id])
+			boto_web.ui.desktop.pages[self.id].listener.update();
 		return boto_web.ui.desktop.pages[self.id];
 	}
 
@@ -52,7 +55,7 @@ boto_web.ui.Page = function(html) {
 		});
 
 		self.node.find(boto_web.ui.selectors.report).each(function() {
-			new boto_web.ui.widgets.Report(this);
+			self.listener = new boto_web.ui.widgets.Report(this);
 		});
 	}
 
