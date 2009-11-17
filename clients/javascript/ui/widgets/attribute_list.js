e3a484f2768222c7eaae3c010fd166f8f3a8c36f
ipaterson <devnull@localhost>
2009-11-17 16:56:17 -0500

    Major overhaul of attributeList widget allows fields to be specified in the bwAttributes property, comma-separated to ensure proper order. Also allows templates for displaying specific attributes.

--- clients/javascript/ui/widgets/attribute_list.js
+++ clients/javascript/ui/widgets/attribute_list.js
@@ -11,22 +11,85 @@
 boto_web.ui.widgets.AttributeList = function(node, model, obj) {
 	var self = this;
 
-	self.node = $(node).addClass('widget-attribute_list');
+	self.node = $(node).addClass('widget-attribute_list clear');
+	self.template = $('<div/>').append(self.node.contents().clone());
+	self.node.empty();
 	self.model = model;
+	self.properties = [];
+	self.sequence = self.node.attr(boto_web.ui.properties.attributes);
 
-	$(self.model.properties).each(function() {
-		var props = this;
+	if (self.sequence && self.sequence != 'all')
+		self.sequence = self.sequence.split(',')
+	else
+		self.sequence = self.model.properties.splice();
 
-		if (typeof obj != 'undefined')
-			props = $.extend(this, {value: obj.properties[this.name]});
+	self.properties = $.map(self.sequence, function(name, num) {
+		if (!(name in self.model.prop_map))
+			return;
+
+		var props = self.model.prop_map[name];
 
 		if (props._perm && $.inArray('read', props._perm) == -1)
 			return;
 
-		var field = boto_web.ui.forms.property_field(props, {read_only: true});
+		//if (!obj.properties[props.name])
+		//	return;
 
-		if (typeof field == 'undefined') return;
+		return props;
+	});
 
-		field.node.appendTo(self.node)
+	/*if (self.properties.length > 20) {
+		self.per_column = Math.ceil(self.properties.length / 3);
+		self.columns = [
+			$('<div/>')
+				.addClass('al p33')
+				.appendTo(self.node),
+			$('<div/>')
+				.addClass('al p33')
+				.appendTo(self.node),
+			$('<div/>')
+				.addClass('al p33')
+				.appendTo(self.node)
+		]
+	}
+	else if (self.properties.length > 5) {*/
+		self.per_column = Math.ceil(self.properties.length / 2);
+		self.columns = [
+			$('<div/>')
+				.addClass('al p50')
+				.appendTo(self.node),
+			$('<div/>')
+				.addClass('al p50')
+				.appendTo(self.node)
+		]
+	/*}
+	else {
+		self.per_column = 1e99;
+		self.columns = [
+			$('<div/>')
+				.appendTo(self.node)
+		]
+	}*/
+
+	$(self.properties).each(function(num, props) {
+		var c = self.columns[Math.floor(num / self.per_column)];
+
+		var template = self.template.find(boto_web.ui.selectors.attribute.replace(']', '=' + props.name + ']'));
+
+		var container = $('<div/>')
+			.addClass('property');
+
+		if (template.length)
+			container.append(template);
+		else
+			container.attr(boto_web.ui.properties.attribute, props.name);
+
+		c.append(
+			$('<label/>')
+				.addClass('property_label')
+				.text(props._label),
+			container,
+			$('<br class="clear"/>')
+		);
 	});
 };
