d6a856c2ffffd63db316e20862b84dc5c45ba81c
cmoyer <devnull@localhost>
2010-08-18 11:08:02 -0400

    Bumped version number

--- botoweb/__init__.py
+++ botoweb/__init__.py
@@ -18,7 +18,7 @@
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
-__version__ = "0.7"
+__version__ = "1.0"
 env = None
 import logging
 log = logging.getLogger("botoweb")
