47114fd702d3e69667788c3c694c0c55366a46c1
cmoyer <devnull@localhost>
2010-11-10 12:29:43 -0500

    Removed calling close on __del___

--- botoweb/response.py
+++ botoweb/response.py
@@ -45,7 +45,3 @@ class Response(webob.Response):
 	def close(self):
 		webob.Response.close(self)
 		self.app_iter.close()
-
-	def __del__(self):
-		if hasattr(self.app_iter, "close"):
-			self.app_iter.close()
