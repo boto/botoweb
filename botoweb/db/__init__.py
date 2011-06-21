3bc2539bf5e2c5e9864b79596ea7a7da16ba7337
cmoyer <devnull@localhost>
2011-06-21 14:54:06 -0400

    Added unescaping any HTML characters before indexing

--- botoweb/db/__init__.py
+++ botoweb/db/__init__.py
@@ -6,9 +6,11 @@
 # and adds a few new features on top of boto.sdb.db
 
 import unicodedata
+import re
 def index_string(s):
 	"""Generates an index of this string,
 	stripping off all accents and making everything upper case"""
 	if not isinstance(s, unicode):
 		s = unicode(s, 'utf-8')
+	s = re.sub(r'&#(x?)([^;]+);', lambda match: unichr(int(match.group(2), 16 if match.group(1) else 10)),s)
 	return unicodedata.normalize('NFKD', s).encode('ASCII', 'ignore').upper()
