940a19f511c2549e60577c4246dc380baddfa6c4
cmoyer <devnull@localhost>
2011-12-28 17:27:09 -0500

    Added "Access-Control-Expose-Headers" option for "allow_headers" if configured for CORS support

--- botoweb/appserver/handlers/__init__.py
+++ botoweb/appserver/handlers/__init__.py
@@ -42,6 +42,11 @@ class RequestHandler(object):
 			response.headers['Access-Control-Allow-Origin'] = str(self.env.config.get('app', 'allow_origin'))
 		if self.config.has_key("allow_origin"):
 			response.headers['Access-Control-Allow-Origin'] = str(self.config.get('allow_origin'))
+		if self.env.config.get("app", "allow_headers"):
+			response.headers['Access-Control-Expose-Headers'] = str(self.env.config.get('app', 'allow_headers'))
+		if self.config.has_key("allow_headers"):
+			response.headers['Access-Control-Expose-Headers'] = str(self.config.get('allow_headers'))
+
 
 		if method in self.allowed_methods:
 			method = getattr(self, "_%s" % method)
