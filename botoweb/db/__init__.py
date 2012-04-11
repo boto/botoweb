42987a6b34f332f0dbc486e28613c4ea45ddbc29
cmoyer <devnull@localhost>
2012-04-11 15:27:18 -0400

    Fixes to index_string unicode handling

--- botoweb/db/__init__.py
+++ botoweb/db/__init__.py
@@ -11,8 +11,8 @@ def index_string(s):
 	"""Generates an index of this string,
 	stripping off all accents and making everything upper case"""
 	if isinstance(s, list):
-		" ".join(s)
+		s = " ".join(s)
 	if not isinstance(s, unicode):
-		s = unicode(s, 'utf-8')
+		s = str(s).decode('utf-8', 'replace')
 	s = re.sub(r'&#(x?)([^;]+);', lambda match: unichr(int(match.group(2), 16 if match.group(1) else 10)),s)
 	return unicodedata.normalize('NFKD', s).encode('ASCII', 'ignore').upper()
