a70150a578322455e69f42a55999f1376128ce35
Chris Moyer <cmoyer@newstex.com>
2013-05-06 16:03:06 -0400

    Bumped to version 1.2

--- botoweb/__init__.py
+++ botoweb/__init__.py
@@ -18,7 +18,7 @@
 # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
-__version__ = "1.1"
+__version__ = "1.2"
 env = None
 import logging
 log = logging.getLogger("botoweb")
